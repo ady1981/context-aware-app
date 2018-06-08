e90773f27191230bd05d4d2243c5e2df440acdca
andrey <andrey@cloudgears.com>
2018-06-08 19:04:01 +0300

    repl added (PASSED)

---
+++ env/dev/context_aware_app/processing_repl.clj
@@ -0,0 +1,27 @@
+(ns context-aware-app.processing-repl
+  (:require
+    [context-aware-app.user.db :as user-db]
+    [context-aware-app.user.entry :as user-entry]
+
+    [context-aware-app.lot.db :as lot-db]
+
+    [context-aware-app.processing :refer :all]))
+
+#_(user-db/enumerate)
+#_(user-db/find-by-id "1")
+
+#_(lot-db/enumerate)
+#_(lot-db/find-by-id "1")
+
+;;;;
+
+#_(user-db/update-by-id! "1" (fn [user] [:ok (update-in user [:wallet :value] - 10)]))
+#_(user-db/update-by-id! "1" (fn [user] :xxx))
+#_(user-db/update-by-id! "1" (fn [user] (/ 1 0)))
+
+;;;;
+
+#_(buy-lot "1" "1")
+#_(buy-lot "1" "2")
+#_(buy-lot "4" "1")
+#_(buy-lot "1" "4")
