fe416717a28a2ec4e682d1e10af93b4eab22ac9b
andrey <andrey@cloudgears.com>
2018-06-09 11:30:48 +0300

    minor

--- src/context_aware_app/processing.clj
+++ src/context_aware_app/processing.clj
@@ -10,24 +10,34 @@
 
 ;; [:ok updated_user] | [:error reason]
 (defn buy-lot [user_id lot_id]
-  (let [with-lot-fn (partial util/with-result-or-error
-                             #(lot-db/find-by-id lot_id)
-                             :lot)
+  (let [with-lot-fn (partial
+                      util/with-result-or-error
+                      #(lot-db/find-by-id lot_id)
+                      :lot)
 
         buy-lot-fn (fn [{:keys [lot] :as ctx}]
-                     (util/with-result-or-error #(user-db/update-by-id! user_id
-                                                                        (fn [user]
-                                                                          (let [wallet_v (get-in user [:wallet :value])
-                                                                                price_v (get-in lot [:price :value])]
-                                                                            (if (>= wallet_v price_v)
-                                                                              (let [updated_user (-> user
-                                                                                                     (update-in [:wallet :value] - price_v)
-                                                                                                     (update-in [:lots] conj {:lot_id lot_id, :price price_v}))]
-                                                                                [:ok updated_user])
-                                                                              [:error {:type :invalid_wallet_value
-                                                                                       :details {:code :not_enough, :provided wallet_v, :required price_v}}]))))
-                                                :user
-                                                ctx))
+                     (util/with-result-or-error
+                       #(user-db/update-by-id!
+                          user_id
+                          (fn [user]
+                            (let [wallet_v (get-in user [:wallet :value])
+                                  price_v (get-in lot [:price :value])]
+                              (if (>= wallet_v price_v)
+                                (let [updated_user (-> user
+                                                       (update-in [:wallet :value]
+                                                                  -
+                                                                  price_v)
+                                                       (update-in [:lots]
+                                                                  conj
+                                                                  {:lot_id lot_id
+                                                                   :price price_v}))]
+                                  [:ok updated_user])
+                                [:error {:type :invalid_wallet_value
+                                         :details {:code :not_enough
+                                                   :provided wallet_v
+                                                   :required price_v}}]))))
+                       :user
+                       ctx))
 
         fs [with-lot-fn
             buy-lot-fn]]
